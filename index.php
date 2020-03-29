d7d9d01dd899d80fb4bfad5a134b7fcb55a5ed3b
rockethyperlocal <mapmydestination@gmail.com>
2020-03-29 16:03:54 +0530

    update

--- index.php
+++ index.php
@@ -5,13 +5,7 @@
 		<link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
 	</head>
 <body>
-	<nav class="navbar navbar-default">
-		<div class="container-fluid">
-			<a class="navbar-brand" href="https://sourcecodester.com">Sourcecodester</a>
-		</div>
-	</nav>
-	<div class="col-md-3"></div>
-	<div class="col-md-6 well">
+
 		<h3 class="text-primary">PHP - Create Excel File From MySQLi Table</h3>
 		<hr style="border-top:1px dotted #ccc;"/>
 		<div class="col-md-4">
@@ -62,6 +56,5 @@
 				</tbody>
 			</table>
 		</div>
-	</div>
 </body>
 </html>
